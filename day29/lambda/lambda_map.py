cd37d01a79ee911095e751eb9aba68f5d50ea0c2
elliajen <dmstndhk123@naver.com>
2021-10-01 13:19:02 +0900

    '소스예제'

--- day29/lambda/lambda_map.py
+++ day29/lambda/lambda_map.py
@@ -1,15 +1,22 @@
 # map(함수, 리스트), filter() 함수와 같이 사용하기
 # list([리스트]) -> 리스트로 반환(출력)
-def time(a):
+def times2(a):
     a2 = [] # 3의 배수를 저장할 빈 리스트
     for i in a:
         a2.append(i * 3)
     return a2
 
 v = [1, 2, 3, 4]
-print(time(v))
-
-
-# 람다함수
 times = lambda x : x * 3
-print(list(map(times, v)))
+result = map(times,v)
+print(list(result))
+print(list(map(lambda x : x * 3, v)))
+#
+# negative = lambda x : x * 3
+# print(times2(v))
+#
+# # 람다함수
+# times = lambda x : x * 3
+# print(list(map(times, v)))
+
+print(times2(v))
