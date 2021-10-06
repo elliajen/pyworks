209c792182e6717e6c95a75346c1d1b94b4a0ba7
elliajen <dmstndhk123@naver.com>
2021-10-06 13:19:06 +0900

    소스

---
+++ gui_ex/tk.py
@@ -0,0 +1,16 @@
+# 방향 - W(west),E(east),S(south),N(north)
+from tkinter import *
+class App:
+    def __init__(self, root):
+        frame = Frame(root)
+        frame.pack()
+
+        Label(frame, text="deg C").grid(row=0, column=0)
+        Button(frame, text="변환").grid(row=1, columnspan=2)
+
+root = Tk()
+root.title("Temp Converter")
+app = App(root)
+
+
+root.mainloop()
